6881a4f07bcd06f12edfee573f5d87a88803acf4
Ivan Urra <ivan.urra.wd@gmail.com>
2022-10-11 17:44:50 +0200

    WIP

--- src/HeroesApp.jsx
+++ src/HeroesApp.jsx
@@ -1,10 +1,8 @@
-import { Navbar } from "./heroes/components/Navbar";
 import { AppRouter } from "./router/AppRouter";
 
 export const HeroesApp = () => {
   return (
     <>
-      <Navbar />
       <AppRouter />
     </>
   );
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,10 +2,12 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import { LoginPage } from "../auth/pages/LoginPage";
 import { DcPage } from "../heroes/pages/DcPage";
 import { MarvelPage } from "../heroes/pages/MarvelPage";
+import { Navbar } from "../ui";
 
 export const AppRouter = () => {
   return (
     <>
+      <Navbar />
       <Routes>
         <Route path="/*" element={<Navigate to="/marvel" />} />
         <Route path="marvel" element={<MarvelPage />} />
