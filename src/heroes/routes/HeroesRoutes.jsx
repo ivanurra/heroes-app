795999fcf3a5c8383bfe88246fb49885aa1a32a4
Ivan Urra <ivan.urra.wd@gmail.com>
2022-10-29 10:35:17 +0200

    WIP

--- src/heroes/routes/HeroesRoutes.jsx
+++ src/heroes/routes/HeroesRoutes.jsx
@@ -1,17 +1,20 @@
-import { Navbar } from "../../ui";
 import { Navigate, Route, Routes } from "react-router-dom";
-import { MarvelPage, DcPage, HeroPage, SearchPage } from "../pages";
+import { Navbar } from "../../ui";
+import { DcPage, HeroPage, MarvelPage, SearchPage } from "../pages";
 
 export const HeroesRoutes = () => {
   return (
     <>
       <Navbar />
+
       <div className="container">
         <Routes>
           <Route path="marvel" element={<MarvelPage />} />
           <Route path="dc" element={<DcPage />} />
+
           <Route path="search" element={<SearchPage />} />
-          <Route path="hero/:heroId" element={<HeroPage />} />
+          <Route path="hero/:id" element={<HeroPage />} />
+
           <Route path="/" element={<Navigate to="/marvel" />} />
         </Routes>
       </div>
