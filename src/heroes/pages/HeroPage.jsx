b29a19611b0fb3a2c35d8e7de0d8fced939a18ef
Ivan Urra <ivan.urra.wd@gmail.com>
2022-10-30 11:00:48 +0100

    WIP

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,10 +1,16 @@
-import { Navigate, useParams } from "react-router-dom";
+import { useParams, Navigate, Route, useNavigate } from "react-router-dom";
 import { getHeroById } from "../helpers";
 
 export const HeroPage = () => {
   const { id } = useParams();
 
+  const navigate = useNavigate();
+
   const hero = getHeroById(id);
+
+  const onNavigateBack = () => {
+    navigate(-1);
+  };
 
   if (!hero) {
     return <Navigate to="/marvel" />;
@@ -18,6 +24,30 @@ export const HeroPage = () => {
           alt={hero.superhero}
           className="img-thumbnail"
         />
+      </div>
+      <div className="col-8">
+        <h1 className="mb-3">{hero.superhero}</h1>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">
+            <b>Alter ego: </b>
+            {hero.alter_ego}
+          </li>
+          <li className="list-group-item">
+            <b>Publisher: </b>
+            {hero.publisher}
+          </li>
+          <li className="list-group-item">
+            <b>First Appearance: </b>
+            {hero.first_appearance}
+          </li>
+          <li className="list-group-item">
+            <b>Characters: </b>
+            {hero.characters}
+          </li>
+        </ul>
+        <button className="btn btn-secondary mt-3" onClick={onNavigateBack}>
+          Back
+        </button>
       </div>
     </div>
   );
