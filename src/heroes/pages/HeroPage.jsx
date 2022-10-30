ba6dd94dd3fcfd92965f2d67c94a214ff51f6072
Ivan Urra <ivan.urra.wd@gmail.com>
2022-10-30 08:40:00 +0100

    WIP

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -11,8 +11,14 @@ export const HeroPage = () => {
   }
 
   return (
-    <>
-      <h1>{hero.superhero}</h1>
-    </>
+    <div className="row mt-5">
+      <div className="col-4">
+        <img
+          src={`/assets/heroes/${id}.jpg`}
+          alt={hero.superhero}
+          className="img-thumbnail"
+        />
+      </div>
+    </div>
   );
 };
