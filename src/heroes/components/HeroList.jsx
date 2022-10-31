82d453edb6766fdf2fd3f88461e9bb8daf9463a0
Ivan Urra <ivan.urra.wd@gmail.com>
2022-10-31 08:25:00 +0100

    WIP

--- src/heroes/components/HeroList.jsx
+++ src/heroes/components/HeroList.jsx
@@ -1,8 +1,9 @@
+import { useMemo } from "react";
 import { HeroCard } from "./HeroCard";
 import { getHeroesByPublisher } from "../helpers";
 
 export const HeroList = ({ publisher }) => {
-  const heroes = getHeroesByPublisher(publisher);
+  const heroes = useMemo(() => getHeroesByPublisher(publisher), [publisher]);
 
   return (
     <div className="row rows-cols-1 row-cols-md-3 g-3">
--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -23,10 +23,10 @@ export const HeroPage = () => {
         <img
           src={`/assets/heroes/${id}.jpg`}
           alt={hero.superhero}
-          className="img-thumbnail"
+          className="img-thumbnail animate__animated animate__fadeInLeft"
         />
       </div>
-      <div className="col-8">
+      <div className="col-8 animate__animated animate__fadeInRight">
         <h1 className="mb-3">{hero.superhero}</h1>
         <ul className="list-group list-group-flush">
           <li className="list-group-item">
