85ba94198b6310ae5165b8fcc293ce6cac3a53f7
Ivan Urra <ivan.urra.wd@gmail.com>
2022-11-02 08:38:28 +0100

    WIP

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,6 +1,17 @@
+import { useForm } from "../../hooks/useForm";
 import { HeroCard } from "../components";
 
 export const SearchPage = () => {
+  const { searchText, onInputChange } = useForm({
+    searchText: "",
+  });
+
+  const onSearchSubmit = (event) => {
+    event.preventDefault();
+    if (searchText.trim().length <= 1) return;
+    console.log({ searchText });
+  };
+
   return (
     <>
       <h1>Search</h1>
@@ -9,13 +20,15 @@ export const SearchPage = () => {
         <div className="col-5">
           <h4>Searching</h4>
           <hr />
-          <form>
+          <form onSubmit={onSearchSubmit}>
             <input
               type="text"
               placeholder="Search a hero"
               className="form-control"
               name="searchText"
               autoComplete="off"
+              value={searchText}
+              onChange={onInputChange}
             />
             <button className="btn btn-outline-primary mt-2">Search</button>
           </form>
