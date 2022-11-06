c63f3613abeaea08e41fe820ebd553a7e85129cf
Ivan Urra <ivan.urra.wd@gmail.com>
2022-11-06 10:41:39 +0100

    WIP

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -12,13 +12,17 @@ export const SearchPage = () => {
 
   const heroes = getHeroesByName(q);
 
+  const showSearch = q.length === 0;
+
+  const showError = q.length > 0 && heroes.length === 0;
+
   const { searchText, onInputChange } = useForm({
     searchText: q,
   });
 
   const onSearchSubmit = (event) => {
     event.preventDefault();
-    if (searchText.trim().length <= 1) return;
+    // if (searchText.trim().length <= 1) return;
 
     navigate(`?q=${searchText}`);
   };
@@ -47,10 +51,31 @@ export const SearchPage = () => {
         <div className="col-7">
           <h4>Results: </h4>
           <hr />
-          <div className="alert alert-primary">Search a hero</div>
-          <div className="alert alert-danger">
+
+          {/* {q === "" ? (
+            <div className="alert alert-primary">Search a hero</div>
+          ) : (
+            heroes.length === 0 && (
+              <div className="alert alert-danger">
+                No results <b>{q}</b>
+              </div>
+            )
+          )} */}
+
+          <div
+            className="alert alert-primary"
+            style={{ display: showSearch ? "" : "none" }}
+          >
+            Search a hero
+          </div>
+
+          <div
+            className="alert alert-danger"
+            style={{ display: showError ? "" : "none" }}
+          >
             No results <b>{q}</b>
           </div>
+
           {heroes.map((hero) => (
             <HeroCard key={hero.id} {...hero} />
           ))}
