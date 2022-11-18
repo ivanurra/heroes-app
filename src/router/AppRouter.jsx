4090009b932dc907cf397b4ae301ec6fe75bc6a6
Ivan Urra <ivan.urra.wd@gmail.com>
2022-11-18 08:14:44 +0100

    WIP

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,14 +1,22 @@
 import { Route, Routes } from "react-router-dom";
 import { LoginPage } from "../auth";
 import { HeroesRoutes } from "../heroes/routes/HeroesRoutes";
-import { Navbar } from "../ui";
+import { PrivateRoute } from "./PrivateRoute";
 
 export const AppRouter = () => {
   return (
     <>
       <Routes>
         <Route path="login" element={<LoginPage />} />
-        <Route path="/*" element={<HeroesRoutes />} />
+        <Route
+          path="/*"
+          element={
+            <PrivateRoute>
+              <HeroesRoutes />
+            </PrivateRoute>
+          }
+        />
+        {/* <Route path="/*" element={<HeroesRoutes />} /> */}
       </Routes>
     </>
   );
