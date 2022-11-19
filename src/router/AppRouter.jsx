3a2653f18102c4ec391579c173b3337e9b3b4e3f
Ivan Urra <ivan.urra.wd@gmail.com>
2022-11-19 11:11:39 +0100

    WIP

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,12 +2,20 @@ import { Route, Routes } from "react-router-dom";
 import { LoginPage } from "../auth";
 import { HeroesRoutes } from "../heroes/routes/HeroesRoutes";
 import { PrivateRoute } from "./PrivateRoute";
+import { PublicRoute } from "./PublicRoute";
 
 export const AppRouter = () => {
   return (
     <>
       <Routes>
-        <Route path="login" element={<LoginPage />} />
+        <Route
+          path="login"
+          element={
+            <PublicRoute>
+              <LoginPage />
+            </PublicRoute>
+          }
+        />
         <Route
           path="/*"
           element={
@@ -16,6 +24,7 @@ export const AppRouter = () => {
             </PrivateRoute>
           }
         />
+        {/* <Route path="login" element={<LoginPage />} /> */}
         {/* <Route path="/*" element={<HeroesRoutes />} /> */}
       </Routes>
     </>
