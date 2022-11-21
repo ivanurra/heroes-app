f47a1f00547aa19c889c71943d3c8cf807244d54
Ivan Urra <ivan.urra.wd@gmail.com>
2022-11-21 08:25:09 +0100

    WIP

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -8,9 +8,11 @@ export const LoginPage = () => {
   const navigate = useNavigate();
 
   const onLogin = () => {
+    const lastPath = localStorage.getItem("lastPath") || "/";
+
     login("Name");
 
-    navigate("/", { replace: true });
+    navigate(lastPath, { replace: true });
   };
 
   return (
